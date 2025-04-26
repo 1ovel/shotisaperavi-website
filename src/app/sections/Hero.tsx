0258fe4c874c397f45288b198d5fa500107d24df
makpet <maksim.petrov.mailbox@gmail.com>
2025-04-26 13:48:27 +0300

    Hero section improvement for smaller screens

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -11,14 +11,14 @@ import { Typography } from '../components/Typography';
 const Hero = () => {
     return (
         <motion.section
-            className="grid lg:gap-xsSpacing grid-cols-12 grid-rows-4 w-full relative flex-1 h-max"
+            className="grid 2xl:gap-xsSpacing grid-cols-12 grid-rows-4 w-full relative flex-1 h-max"
             variants={containerVariants}
             initial="hidden"
             animate="visible"
         >
             <motion.div
                 variants={itemVariants}
-                className="hidden lg:block col-span-2 col-start-4 row-start-3 row-span-2"
+                className="hidden 2xl:block col-span-2 col-start-4 row-start-3 row-span-2"
             >
                 <Image
                     src={imageFood}
@@ -29,7 +29,7 @@ const Hero = () => {
 
             <motion.div
                 variants={itemVariants}
-                className="col-span-6 col-start-7 lg:col-span-4 lg:col-start-6 row-start-1 row-span-4"
+                className="col-span-6 col-start-7 2xl:col-span-4 2xl:col-start-6 row-start-1 row-span-4"
             >
                 <Image
                     src={imageBread}
@@ -39,18 +39,18 @@ const Hero = () => {
             </motion.div>
 
             <div className="grid leading-none gap-x-xsSpacing grid-cols-12 grid-rows-[90px-90px-90px] sx:grid-rows-[140px_140px_140px] w-full absolute top-1/2 -translate-y-1/2">
-                <motion.div variants={itemVariants} className="lg:col-start-3">
+                <motion.div variants={itemVariants} className="2xl:col-start-3">
                     <Typography variant="h1">Authentic</Typography>
                 </motion.div>
                 <motion.div
                     variants={itemVariants}
-                    className="lg:col-start-7 row-start-2 row-auto"
+                    className="2xl:col-start-7 row-start-2 row-auto"
                 >
                     <Typography variant="h1">Georgian</Typography>
                 </motion.div>
                 <motion.div
                     variants={itemVariants}
-                    className="lg:col-start-5 row-start-3"
+                    className="2xl:col-start-5 row-start-3"
                 >
                     <Typography variant="h1">Tastes</Typography>
                 </motion.div>
@@ -68,7 +68,7 @@ const Hero = () => {
                 </Typography>
                 <Typography
                     variant="decoration"
-                    className="absolute bottom-0 left-0 lg:left-auto lg:right-0"
+                    className="absolute bottom-0 left-0 2xl:left-auto 2xl:right-0"
                 >
                     საკვები
                 </Typography>
